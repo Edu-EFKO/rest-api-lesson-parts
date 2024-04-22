49032bfb15a922759387eaa0d463fdccc895031d
Andrey Bogachevskiy <ace7upp@gmail.com>
2024-04-22 10:43:35 +0300

    инициализация проекта

---
+++ components/filters/TokenAuthMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+
+use yii\base\ActionFilter;
+use yii\web\UnauthorizedHttpException;
+
+class TokenAuthMiddleware extends ActionFilter
+{
+    public function beforeAction($action): true
+    {
+        $token = \Yii::$app->request->headers->get('X-Api-Key');
+
+        if ($token !== getenv('X_API_KEY')) {
+            throw new UnauthorizedHttpException('Авторизация не выполнена');
+        }
+
+        return true;
+    }
+}
